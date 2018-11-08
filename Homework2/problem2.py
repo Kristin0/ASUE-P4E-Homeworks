0f1ab31a815cb57b6083ea96f18b35340a49c18e
Kristin0 <44585557+Kristin0@users.noreply.github.com>
2018-11-08 16:54:16 +0400

    Update problem2.py

--- Homework2/problem2.py
+++ Homework2/problem2.py
@@ -1,18 +1,26 @@
   1 import random
   2 
   3 num = random.randint(1,10)
-  4 guess = input("Enter an integer: ")
+  4 guess = input("Enter your number: ")
   5 counter = 0
-  6 while int(guess) != num:
-  7     if guess == "exit":
-  8         exit(0)
-  9     elif int(guess) > num:
- 10         guess = (input("Enter lower: "))
- 11         counter += 1
- 12     elif int(guess) < num:
- 13         guess = (input("Enter higher: "))
- 14         counter += 1
- 15 #    elif guess == "exit":
- 16 #        break
- 17 print("Right!It's " + str(guess) + " with " + str(counter) + "d attemp")
+  6 while guess != "exit" and int(guess) != num:
+  7     if int(guess) > num:
+  8         guess = input("Enter lower: ")
+  9         counter += 1
+ 10     elif int(guess) < num:
+ 11         guess = input("Enter higher: ")
+ 12         counter += 1
+ 13 if guess == "exit":
+ 14     print("Exit")
+ 15 else:
+ 16     print("Right!It's " + str(guess) + " with " + str(counter) + "d attemp")
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+                  
+
 
